3e5dddbbb52e18a9ec597741eab84ec321b94e62
Dorthu <dorthu2@gmail.com>
2015-09-13 09:21:48 -0400

    Removed ambiguous constant definition

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,17 @@
 """
 hello_world - a program to print the phrase 'hello world'
 """
-MESSAGE = 'hello world'
+def get_word(word_number):
+    if word_number == 0:
+        return 'hello'
+    elif word_number == 1:
+        return 'world'
+    else:
+        raise AttributeError("Word index out of range (only 0 and 1 are allowed)")
 
 def display_text(text):
     print(text)
 
+MESSAGE = "{} {}".format(get_word(0), get_word(1))
 
 display_text(MESSAGE)
